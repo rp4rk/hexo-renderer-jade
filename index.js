990a68bbf9aeef2212fd3e392550ba97f9e8115c
Ryan Park <admin@ryanpark.co.uk>
2015-11-29 21:20:28 +0000

    Removed console logging

--- index.js
+++ index.js
@@ -4,20 +4,14 @@ hexo.extend.renderer.register('jade', 'html', function(data, locals){
   
   // Get config
   var config = hexo.theme.config.jade;
-  
   var options = {
     filename: data.path
   };
-  
-  
-  
+
   // Append config to options
   for (var k in config) {
     options[k] = config[k];
   }
-  
-  console.log(this.config.jade);
-  console.log(options);
   
   // Compile
   return jade.compile(data.text, options)(locals);
